c1154549b711be9bb84d4324a835493a4a81c95b
peng-xiao-shuai <1612565136@qq.com>
2023-02-15 17:23:03 +0800

    修复：window is no defined

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,20 +1,24 @@
 import DefaultTheme from 'vitepress/theme';
-import ElementPlus from 'element-plus';
-import PowerfulTable from 'el-plus-powerful-table-ts';
-import 'element-plus/dist/index.css';
-import 'el-plus-powerful-table-ts/lib/es/style.css';
+import 'element-plus/theme-chalk/src/index.scss';
+import 'element-plus/theme-chalk/dark/css-vars.css';
+// import 'el-plus-powerful-table-ts/es/style.css';
 import './styles/index.scss';
 import { VPDemo, VPLayout, VPApiTyping } from '../components/index';
 export default {
   ...DefaultTheme,
   Layout: VPLayout,
   enhanceApp({ app, router, siteData }) {
-    // app is the Vue 3 app instance from `createApp()`. router is VitePress'
-    // custom router. `siteData`` is a `ref`` of current site-level metadata.
     app.component('Demo', VPDemo);
     app.component('ApiTyping', VPApiTyping);
-    app.use(ElementPlus);
-    app.use(PowerfulTable);
-    console.log(app);
+    import('element-plus').then((module) => {
+      app.use('ElementPlus', module);
+    });
+    if (typeof window !== 'undefined') {
+      import('el-plus-powerful-table-ts/lib').then((module) => {
+        console.log(module);
+
+        app.use('PowerfulTable', module);
+      });
+    }
   },
 };
