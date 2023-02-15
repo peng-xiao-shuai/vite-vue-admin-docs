a01594ca6cf42437a59a9aa3066490e9abf1059e
peng-xiao-shuai <1612565136@qq.com>
2023-02-15 17:23:46 +0800

    修改：文件夹重命名

---
+++ examples/powerful-table-demo/basic.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <PowerfulTable ref="PowerfulTable" :list="lists" isSelect :header="headers"> -->
+  <!-- </PowerfulTable> -->
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref, onMounted } from 'vue';
+import type { PowerfulTableHeader } from 'el-plus-powerful-table-ts';
+// import { lists, header } from './index';
+// import type { Lists } from './index';
+
+// const list = ref<Lists[]>(lists);
+// const headers: PowerfulTableHeader<Lists[0]>[] = reactive(header);
+const listQuery = reactive({
+  pageNum: 1,
+  pageSize: 2,
+});
+
+const getList = (query = {}) => {
+  Object.assign(listQuery, query);
+
+  // setTimeout(() => {
+  //   list.value = lists.filter(
+  //     (item, index) =>
+  //       index >= (listQuery.pageNum - 1) * listQuery.pageSize &&
+  //       index < listQuery.pageNum * listQuery.pageSize
+  //   );
+  // });
+};
+
+onMounted(() => {
+  getList();
+});
+</script>
