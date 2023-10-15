7cbe65053bb8c273cd5ed1ee50d536721c12e502
peng-xiao-shuai <1612565136@qq.com>
2023-10-15 13:49:52 +0800

    fix: require 改为 import

--- utils/highlight.ts
+++ utils/highlight.ts
@@ -1,45 +1,44 @@
-import prism from 'prismjs'
-
-// prism is listed as actual dep so it's ok to require
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const loadLanguages = require('prismjs/components/index')
+import prism from 'prismjs';
+import loadLanguages from 'prismjs/components/index';
 
 // 加载默认语言
-loadLanguages(['markup', 'css', 'javascript'])
+loadLanguages(['markup', 'css', 'javascript']);
 
 export const highlight = (str: string, lang: string) => {
   if (!lang) {
-    return `<pre v-pre><code>${str}</code></pre>`
+    return `<pre v-pre><code>${str}</code></pre>`;
   }
-  // 转小写
-  lang = lang.toLowerCase()
+  // // 转小写
+  lang = lang.toLowerCase();
   switch (lang) {
     case 'text':
-      lang = 'textile'
+      lang = 'textile';
       break;
     case 'vue':
     case 'html':
-      lang = 'markup'
+      lang = 'markup';
       break;
     case 'md':
-      lang = 'markdown'
+      lang = 'markdown';
       break;
     case 'ts':
-      lang = 'typescript'
+      lang = 'typescript';
       break;
   }
   // 匹配到没有默认加载的语言，在进行加载
   if (!prism.languages[lang]) {
     try {
-      loadLanguages([lang])
+      loadLanguages([lang]);
     } catch {
-      console.log(`[vitepress] Syntax highlight for language "${lang}" is not supported.`);
+      console.log(
+        `[vitepress] Syntax highlight for language "${lang}" is not supported.`
+      );
     }
   }
   if (prism.languages[lang]) {
-    const code = prism.highlight(str, prism.languages[lang], lang)
+    const code = prism.highlight(str, prism.languages[lang], lang);
 
-    return `<pre v-pre><code>${code}</code></pre>`
+    return `<pre v-pre><code>${code}</code></pre>`;
   }
-  return `<pre v-pre><code>${str}</code></pre>`
-}
+  return `<pre v-pre><code>${str}</code></pre>`;
+};
