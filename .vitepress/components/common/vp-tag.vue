ea2d0c0e93196df32fcc3d8c58eb24bbd4f2497f
peng-xiao-shuai <1612565136@qq.com>
2023-10-15 22:41:54 +0800

    feat(component): vp-tag

---
+++ .vitepress/components/common/vp-tag.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+defineProps({
+  type: String as PropType<'' | 'success' | 'warning' | 'info' | 'danger'>,
+  content: String,
+});
+</script>
+
+<template>
+  <ClientOnly>
+    <ElTag size="small" :type="type" class="mx-1" effect="light" round>{{
+      content
+    }}</ElTag>
+  </ClientOnly>
+</template>
--- .vitepress/components/index.ts
+++ .vitepress/components/index.ts
@@ -1,3 +1,4 @@
 export { default as VPDemo } from './common/vp-demo.vue';
 export { default as VPLayout } from './common/vp-layout.vue';
 export { default as VPApiTyping } from './common/vp-api-typing.vue';
+export { default as VPTag } from './common/vp-tag.vue';
