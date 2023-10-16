b7dc35cfda6a6962774f373440f2aebef1905c5b
peng-xiao-shuai <1612565136@qq.com>
2023-10-16 22:07:14 +0800

    feat: 补充jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import { MarkdownTransform } from './utils/markdown-transform';
 
 export default defineConfig(async ({ mode }) => {
   return {
-    plugins: [MarkdownTransform()],
+    plugins: [vueJsx(), MarkdownTransform()],
     server: {
       host: true,
     },
